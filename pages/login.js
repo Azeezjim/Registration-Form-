cbb7cdb070291aeb723e65e62d004183abd917fd
ore291 <oreoluwapadonu@gmail.com>
2022-03-22 22:06:31 +0100

    .

--- pages/login.js
+++ pages/login.js
@@ -17,26 +17,31 @@ const login = () => {
   
   };
   return (
-      <>
-        <div className="pt-16">
+    
+        <div className="pt-[75px]">
 
-    <div className="bg-cover w-full  rounded-2xl h-[500px] bg-[url('/banner-mobile.jpg')]" >
-        <div className="flex-col flex items-center justify-center text-white text-center">
-            <div className="w-40 h-20 relative">
+    <div className="bg-cover w-full  rounded-3xl h-[500px] bg-[url('/banner-mobile.jpg')] bg-playRed/40 bg-blend-darken" >
+        <div className="flex-col flex items-center space-y-4 justify-between text-white text-center p-5 ">
+            <div className="w-40 h-10 relative">
               <Image src={'/logo-mobile.png'} layout="fill" objectFit="contain" />
             </div>
-            <h1 className="text-3xl font-bold">
+           <div>
+           <h1 className="text-3xl font-bold">
               Create Original Content
             </h1>
             <p className="text-lg font-semibold">
               for your passionate fans and get paid for it
             </p>
-            <div className="w-40 h-10 rounded-3xl flex iems-center justify-center bg-text-playRed">
-              <span>Register</span>
+           </div>
+            <div className="w-60 h-10 rounded-3xl flex items-center justify-center bg-playRed cursor-pointer">
+              <span className="font-semibold">Register</span>
             </div>
             <h1 className="text-4xl font-semibold">
               JOIN CREATE EARN
             </h1>
+            <p className="text-xs">
+              Playjor is an exclusive content and communication tool for all influncers like youtubers, fitness trainers, models, content creators, public figures and social media influencers, who can connect and share their content
+            </p>
         </div>
     </div>
 
@@ -45,7 +50,7 @@ const login = () => {
       <input type="password" ref={passwordRef} name="password" id="password" />
       <button onClick={userLogin}>Sign in</button>
     </div>
-      </>
+     
   
   );
 };
