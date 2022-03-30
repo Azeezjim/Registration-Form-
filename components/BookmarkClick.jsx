4c581fb5efc4cdad10f466be53263ec5b13a8d66
Azeezjim <azeexjim@gmail.com>
2022-03-30 11:49:27 -1100

    4th commit of the day

--- components/BookmarkClick.jsx
+++ components/BookmarkClick.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   MdOutlinePhotoSizeSelectActual,
   MdOutlineVideoCameraFront,
@@ -9,14 +10,22 @@ function BookmarkClick() {
     <div>
       <div className="bg-white p-2">
         <div className="space-y-4 text-xs flex justify-center ml-4 mt-[-10px] flex-col">
-          <span className="flex space-x-4 text-gray-600 ">
-            <MdOutlinePhotoSizeSelectActual className="mt-1 font-semibold" />
-            <p>Photos</p>
-          </span>
-          <span className="flex space-x-4 ">
-            <MdOutlineVideoCameraFront className="mt-1 font-semibold" />
-            <p>Videos</p>
-          </span>
+          <Link href="/bookmarks/photos" replace>
+            <a>
+              <span className="flex space-x-4 text-gray-600 ">
+                <MdOutlinePhotoSizeSelectActual className="mt-1 font-semibold" />
+                <p>Photos</p>
+              </span>
+            </a>
+          </Link>
+          <Link  href="/bookmarks/videos" replace>
+            <a>
+              <span className="flex space-x-4 ">
+                <MdOutlineVideoCameraFront className="mt-1 font-semibold" />
+                <p>Videos</p>
+              </span>
+            </a>
+          </Link>
           <span className="flex space-x-4 ">
             <AiOutlineAudio className="mt-1 font-semibold" />
             <p>Audios</p>
