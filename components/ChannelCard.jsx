226f4c747d0af1f0c7a6a6115b208016f1071aaa
ore291 <oreoluwapadonu@gmail.com>
2022-03-31 13:40:06 +0100

    .

--- components/ChannelCard.jsx
+++ components/ChannelCard.jsx
@@ -1,30 +1,95 @@
 import Image from "next/image";
 import Button from "./Button";
+import { BsHeartFill, BsEyeFill } from "react-icons/bs";
 
-const ChannelCard = () => {
+const ChannelCard = ({ main }) => {
+  if (main) {
     return (
-       <div className="w-52 h-80 bg-white rounded-xl">
-           <div className="relative w-full h-[90px] rounded-t-lg mb-16">
+      <div className=" w-56 h-[230px]  border rounded-xl shadow-md overflow-hidden flex flex-col relative group cursor-pointer flex-shrink-0 ">
+        <div className="relative w-full h-40 rounded-t-lg ">
           <Image
-            src={"https://stackdiary.com/140x100.png"}
+            src={"https://picsum.photos/200/200?random=3"}
             objectFit="cover"
             layout="fill"
             className="rounded-t-lg"
           />
-          <div className="absolute -bottom-[65%] left-1/3">
-            <div className="w-16 h-16 md:w-20 md:h-20 relative">
+          <div className="w-10 h-6 bg-[#D43D7D]/90 rounded-sm row-container p-1 absolute top-2 left-2">
+              <span className="text-xs text-white">Hot</span>
+
+          </div>
+        </div>
+        <div className="p-2">
+          <div className="flex items center justify-start">
+            <div className="col-container space-y-1">
+              <span className="text-xl text-gray-700 font-bold">
+                Best place
+              </span>
+              <span className="text-sm font-semibold text-gray-400">
+                Bessie howard
+              </span>
+            </div>
+          </div>
+          <div className="flex items center justify-end space-x-1">
+            <div className="row-container space-x-1">
+              <BsHeartFill className="w-3 h-3 text-gray-300" />
+              <span className="text-sm text-gray-300">42</span>
+            </div>
+            <div className="row-container space-x-1">
+              <BsEyeFill className="w-4 h-4 text-gray-300" />
+              <span className="text-sm text-gray-300">114</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="first:ml-auto last:mr-auto w-56 h-[300px] mb-5 border rounded-xl shadow-md overflow-hidden flex flex-col relative group cursor-pointer flex-shrink-0 space-y-3">
+      <div className="relative w-full h-[90px] rounded-t-lg mb-16">
+        <Image
+          src={"https://picsum.photos/200/200?random=3"}
+          objectFit="cover"
+          layout="fill"
+          className="rounded-t-lg"
+        />
+        <div className="absolute -bottom-16 left-[50px]">
+          <div className="row-container bg-white p-1 rounded-3xl">
+            <div className="w-16 h-16 md:w-28 md:h-28 relative rounded-3xl">
               <Image
-                src="/profile_avatar_full.jpg"
+                src="https://picsum.photos/200/200?random=3"
                 alt="side-img"
                 layout="fill"
                 objectFit="cover"
-                className="relative rounded-full"
+                className="rounded-3xl"
               />
             </div>
           </div>
         </div>
-       </div>
-    )
-}
+      </div>
+      <div className="col-container w-full ">
+        <p className="font-semibold text-center text-lg">Fashionista</p>
+        <div className="row-container space-x-1 ">
+          <span className="border-r-2 border-black pr-10  font-bold text-sm">
+            4
+          </span>
+          <span className="pl-10 font-bold text-sm">4</span>
+        </div>
+        <div className="row-container space-x-1  ">
+          <span className="pr-2 font-semibold text-gray-400">Followers</span>
+          <span className="pl-3 font-semibold text-gray-400">Subscribers</span>
+        </div>
+      </div>
+      <div className="row-container px-3">
+        <Button
+          text="Subscribe N2,000"
+          textClass="text-sm text-gray-100 font-semibold"
+          extraClasses="w-full h-8"
+          active={true}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default ChannelCard
+export default ChannelCard;
--- pages/channels.js
+++ pages/channels.js
@@ -1,19 +1,25 @@
 import SideNavLayout from "../components/SideNavLayout";
 import ChannelTabs from "../components/ChannelTabs";
-
+import ChannelCard from "../components/ChannelCard";
 
 const channels = () => {
-    return (
-        <SideNavLayout>
-        <div className="max-w-[950px] px-1 mx-auto">
-            <div className="p-5 my-3 lg:my-6  bg-white rounded-xl shadow-lg  outline-none">
-                <h1 className="text-3xl font-semibold my-5">Channels</h1>
-                <ChannelTabs />
-            </div>
-          
+  return (
+    <SideNavLayout>
+      <div className="max-w-[950px] px-1 mx-auto">
+        <div className="p-5 my-3 lg:my-6  bg-white rounded-xl shadow-lg  outline-none">
+          <h1 className="text-3xl font-semibold my-5">Channels</h1>
+          <ChannelTabs />
         </div>
-      </SideNavLayout>
-    )
-}
+        <div className="p-5 my-3 lg:my-6  bg-white rounded-xl shadow-lg  outline-none">
+          <div className="grid cols-1 lg:grid-cols-4 md:grid-cols-3 gap-4">
+            {[...Array(10)].map((_, index) => (
+              <ChannelCard key={index} main={true}/>
+            ))}
+          </div>
+        </div>
+      </div>
+    </SideNavLayout>
+  );
+};
 
-export default channels
+export default channels;
