b2980b4304554f0f24656965b2a4966068d50421
ore291 <oreoluwapadonu@gmail.com>
2022-03-31 11:27:38 +0100

    .

---
+++ components/ChannelCard.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Button from "./Button";
+
+const ChannelCard = () => {
+    return (
+       <div className="w-40 h-80 bg-white rounded-xl">
+           <div className="relative w-full h-[90px] rounded-t-lg mb-16">
+          <Image
+            src={"https://stackdiary.com/140x100.png"}
+            objectFit="cover"
+            layout="fill"
+            className="rounded-t-lg"
+          />
+          <div className="absolute -bottom-[65%] left-1/3">
+            <div className="w-16 h-16 md:w-20 md:h-20 relative">
+              <Image
+                src="/profile_avatar_full.jpg"
+                alt="side-img"
+                layout="fill"
+                objectFit="cover"
+                className="relative rounded-full"
+              />
+            </div>
+          </div>
+        </div>
+       </div>
+    )
+}
+
+export default ChannelCard
---
+++ components/ChannelTabs.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Tab } from "@headlessui/react";
+import ChannelCard from "./ChannelCard";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const ChannelTabs = () => {
+  let [categories] = useState([
+    "Subscribed channels",
+    "Suggested Channels",
+    "My Channel",
+  ]);
+
+  return (
+    <Tab.Group>
+      <div className="bg-white rounded-2xl p-1 mb-10">
+        <Tab.List>
+          <div className="flex justify-start space-x-3 items-center">
+            {categories.map((category, index) => (
+              <Tab
+                key={index}
+                className={({ selected }) =>
+                  classNames(
+                    "w-auto h-8 px-4 text-sm leading-5 font-medium ",
+                    "focus:outline-none focus:ring-0 ",
+                    selected
+                      ? "bg-[#FFCFD4] text-textPlayRed rounded-full shadow-lg"
+                      : "text-gray-800 bg-white rounded-full shadow-lg"
+                  )
+                }
+              >
+                {category}
+              </Tab>
+            ))}
+          </div>
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          {/* users tab */}
+          <Tab.Panel className={classNames("bg-white rounded-xl p-1")}>
+            <div className="flex space-x-2 overflow-x-scroll">
+              
+            </div>
+              {[...Array(10)].map((_, index) => (
+                <ChannelCard key={index} />
+              ))}
+
+          </Tab.Panel>
+          {/* channels tab */}
+          <Tab.Panel className={classNames("bg-white rounded-xl p-1")}>
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-x-20 gap-y-4"></div>
+            <div className="w-full row-container space-x-1 py-5">
+              <div className="!bg-white border !w-8 !h-8 shadow hover:shadow-2xl icon-bg"></div>
+              <p className="text-lightPlayRed font-medium text-sm cursor-pointer">
+                Load more Channels
+              </p>
+            </div>
+          </Tab.Panel>
+          <Tab.Panel className={classNames("bg-white rounded-xl p-3")}>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-5"></div>
+            <div className="w-full row-container space-x-1 py-5">
+              <div className="!bg-white border !w-8 !h-8 shadow hover:shadow-2xl icon-bg"></div>
+              <p className="text-lightPlayRed font-medium text-sm cursor-pointer">
+                Load more Groups
+              </p>
+            </div>
+          </Tab.Panel>
+        </Tab.Panels>
+      </div>
+    </Tab.Group>
+  );
+};
+
+export default ChannelTabs;
