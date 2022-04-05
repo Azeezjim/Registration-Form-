4ce3c9264c1eda4a9744b9ed87be712d2145b67c
Azeezjim <azeexjim@gmail.com>
2022-04-04 22:10:39 -1100

    latest commit of the day

--- components/ProfileNavBar.jsx
+++ components/ProfileNavBar.jsx
@@ -19,23 +19,23 @@ function ProfileNavItem() {
   const handleClick = () => setShowBookmark(!showBookmark);
 
   return (
-    <div className="mt-16 w-1/6">
-      <div className="pr-5 py-5 block bg-white rounded shadow-sm">
-        <div className="space-y-3 ml-5 font-semibold">
-          <span className="flex cursor-pointer space-x-4">
+    <div className="mt-16 w-1/5">
+      <div className="pr-7 py-7 block bg-white rounded shadow-sm">
+        <div className="space-y-4 ml-7 font-semibold">
+          <span className="flex cursor-pointer space-x-5">
             <RiBarChartHorizontalFill className="mt-1 font-semibold"/>
             <p>Dashboard</p>
           </span>
-          <span className="flex space-x-4 cursor-pointer">
+          <span className="flex space-x-5 cursor-pointer">
             <BiUserCircle className="mt-1 font-semibold" />
             <p>Profile</p>
           </span>
-          <span className="flex space-x-4 cursor-pointer">
+          <span className="flex space-x-5 cursor-pointer">
             <BiHistory className="mt-1 font-semibold" />
             <p>Stories</p>
           </span>
 
-          <span className="flex space-x-4 cursor-pointer" onClick={handleClick}>
+          <span className="flex space-x-5 cursor-pointer" onClick={handleClick}>
             <BsBookmark className="mt-1 font-semibold" />
             <p>Bookmarks</p>
           </span>
@@ -43,36 +43,36 @@ function ProfileNavItem() {
             <BookmarkClick className="absolute  top-5 right-0" />
           )}
 
-          <span className="flex space-x-4 cursor-pointer">
+          <span className="flex space-x-5 cursor-pointer">
             <MdWifiCalling className="mt-1 font-semibold" />
             <p>Calls</p>
           </span>
 
-          <span className="flex space-x-4 cursor-pointer">
+          <span className="flex space-x-5 cursor-pointer">
             <BsListStars className="mt-1 font-semibold" />
             <p>Lists</p>
           </span>
-          <span className="flex space-x-4 cursor-pointer">
+          <span className="flex space-x-5 cursor-pointer">
             <VscReferences className="mt-1 font-semibold" />
             <p>Referrals</p>
           </span>
           {/* <Link href="/market" replace> */}
             {/* <a> */}
-              <span className="flex space-x-4 cursor-pointer">
+              <span className="flex space-x-5 cursor-pointer">
                 <FaSuitcase className="mt-1 font-semibold" />
                 <p>Marketing</p>
               </span>
             {/* </a> */}
           {/* </Link> */}
-          <span className="flex space-x-4 cursor-pointer">
+          <span className="flex space-x-5 cursor-pointer">
             <MdOutlinePayment className="mt-1 font-semibold" />
             <p>Payments</p>
           </span>
-          <span className="flex space-x-4 cursor-pointer">
+          <span className="flex space-x-5 cursor-pointer">
             <GrShieldSecurity className="mt-1 font-semibold" />
             <p>Security</p>
           </span>
-          <span className="flex space-x-4 cursor-pointer">
+          <span className="flex space-x-5 cursor-pointer">
             <MdOutlineContactSupport className="mt-1 font-semibold text-base" />
             <p>Help and Support </p>
           </span>
