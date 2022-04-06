144306cc930a8039de45e6b62d8f3245c99b4d19
Azeezjim <azeexjim@gmail.com>
2022-04-06 03:38:52 -1100

    latest commit of the day

--- components/OrderList.jsx
+++ components/OrderList.jsx
@@ -4,12 +4,69 @@ function OrderList() {
   const dataArray = [
     {
       image: "https://i.ibb.co/vJWQbZy/1.jpg",
-    }
-  ]
+      item_name: "Pink Dress",
+      quantity: "Quantity 1",
+      id: "0-12-3e6376-784",
+      address: "Kudiu Gati, Hosur Road, Banglone, Kankataka",
+      phone: 91346451287,
+      amount: "$1629.00",
+      action: "view",
+    },
+    {
+      image: "https://i.ibb.co/vJWQbZy/1.jpg",
+      item_name: "Stretch Brand",
+      quantity: "Quantity 4",
+      id: "0-12-3e6376-784",
+      address: "Kudiu Gati, Hosur Road, Banglone, Kankataka",
+      phone: 91346451287,
+      amount: "$1629.00",
+      action: "view",
+    },
+    {
+      image: "https://i.ibb.co/vJWQbZy/1.jpg",
+      item_name: "Stretch Brand",
+      quantity: "Quantity 8",
+      id: "0-12-3e6376-784",
+      address: "Kudiu Gati, Hosur Road, Banglone, Kankataka",
+      phone: 91346451287,
+      amount: "$1629.00",
+      action: "view",
+    },
+    {
+      image: "https://i.ibb.co/vJWQbZy/1.jpg",
+      item_name: "Stretch Brand",
+      quantity: "Quantity 12",
+      id: "0-12-3e6376-784",
+      address: "Kudiu Gati, Hosur Road, Banglone, Kankataka",
+      phone: 91346451287,
+      amount: "$1629.00",
+      action: "view",
+    },
+    {
+      image: "https://i.ibb.co/vJWQbZy/1.jpg",
+      item_name: "Stretch Brand",
+      quantity: "Quantity 4",
+      id: "0-12-3e6376-784",
+      address: "Kudiu Gati, Hosur Road, Banglone, Kankataka",
+      phone: 91346451287,
+      amount: "$1629.00",
+      action: "view",
+    },
+    {
+      image: "https://images.pexels.com/photos/6752172/pexels-photo-6752172.jpeg?cs=srgb&dl=pexels-rachel-claire-6752172.jpg&fm=jpg",
+      item_name: "Stretch Brand",
+      quantity: "Quantity 24",
+      identification: "0-12-3e6376-784",
+      address: "Kudiu Gati, Hosur Road, Banglone, Kankataka",
+      phone: 91346451287,
+      amount: "$1629.00",
+      action: "view",
+    },
+  ];
 
   return (
-    <div>
-      <div className="p-2.5 bg-red-500 text-white flex justify-between text-[12px]">
+    <div className="">
+      <div className="p-2.5 bg-lightPlayRed  text-white flex justify-between text-[12px]">
         <p>Product</p>
         <div className="flex justify-between space-x-[4.5rem] mr-20">
           <p className="mr-7">Order id</p>
@@ -19,28 +76,38 @@ function OrderList() {
           <p>Action</p>
         </div>
       </div>
+      <div className="">
+        {dataArray.map((data, index) => (
+          <div
+            key={index}
+            className="flex justify-between space-x-[4.5rem] mr-20  space-y-3 text-[12px]"
+          >
+            {console.log("data", data)}
+            <div className="flex justify-between items-center text-[10px] space-x-3 mt-5 ">
+              {/* <div className="p-2.5 flex justify-between text-[12px]"> */}
+              <img
+                src={data.image}
+                alt="Order image"
+                className="w-32 h-[100px] " 
+              />
 
-      <div className="flex justify-between text-[12px] mt-8">
-        <div className="flex space-x-3">
-          <img
-            src="images/Illustration/football.jpg"
-            alt="Order image"
-            className="w-32 h-[100px] flex justify-start"
-          />
-          <div className="flex flex-col justify-center items-center">
-            <p>Pink Dress</p>
-            <p>Quantity 1</p>
+              <div className="flex flex-col justify-center items-center">
+                <p>{data.item_name}</p>
+                <p>{data.quantity}</p>
+              </div>
+            </div>
+            <div className="flex justify-between items-center space-x-[4.5rem] ">
+              <p>{data.id}</p>
+              <p className="w-20">{data.address}</p>
+              <p>{data.phone}</p>
+              <p>$0.00</p>
+              <button className="text-white bg-red-500 py-1 px-5 rounded-md text-[11px]">
+                {data.action}
+              </button>
+            </div>
           </div>
-        </div>
-      <div className="flex justify-between items-center space-x-[4.5rem] mr-20">
-        <p>0-23-64879869</p>
-        <p className="w-20">Kudiu Gati, Hosur Road, Banglone, Kankataka </p>
-        <p>917834537</p>
-        <p>05673874</p>
-        <button className="text-white bg-red-500 py-1 px-5 rounded-md text-[11px]">
-            View
-          </button>
-      </div>
+          // </div>
+        ))}
       </div>
     </div>
   );
