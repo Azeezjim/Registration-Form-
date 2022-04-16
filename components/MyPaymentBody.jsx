9d3c60dcb55cc3ca57bcc02893b3ce8a275683e1
Azeezjim <azeexjim@gmail.com>
2022-04-16 02:20:13 -1100

    latest commit of the day

--- components/MyPaymentBody.jsx
+++ components/MyPaymentBody.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function MyPaymentBody() {
   return (
-    <div>
-      
+    <div className="bg-white border drop-shadow-lg width-full rounded-lg" >
+      <div className='grid grid-cols-4 justify-center'>
+        <p>Paid to</p>
+        <p>Date</p>
+        <p>Amount</p>
+        <p>Type</p>
+      </div>
+      <hr />
     </div>
   )
 }
